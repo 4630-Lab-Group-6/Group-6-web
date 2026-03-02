2b139344d2c4259acacbca354167e51e7de0ae86
YorkFan4925 <106598577+YorkFan4925@users.noreply.github.com>
2026-03-01 23:50:57 -0500

    Update Catalog.tsx

--- frontend/src/Catalog/Catalog.tsx
+++ frontend/src/Catalog/Catalog.tsx
@@ -1,25 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { catalogData } from "./data";
 
-function Catalog() {
+type Product = {
+  _id: number;
+  name: string;
+  image: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numReviews: number;
+};
+
+const products: Product[] = [
+  {
+    _id: 1,
+    name: "Slim Shirt",
+    image: "/images/d1.jpg",
+    brand: "Nike",
+    price: 60,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: 2,
+    name: "Fit Shirt",
+    image: "/images/d1.jpg",
+    brand: "Adidas",
+    price: 55,
+    rating: 4.2,
+    numReviews: 8,
+  },
+  {
+    _id: 3,
+    name: "Best Pants",
+    image: "/images/d1.jpg",
+    brand: "Puma",
+    price: 75,
+    rating: 4.8,
+    numReviews: 15,
+  },
+];
+
+export default function Catalog() {
   return (
     <div className="content">
+      <h2>Catalog</h2>
+
       <ul className="products">
-        {catalogData.products.map((p) => (
+        {products.map((p) => (
           <li key={p._id}>
             <div className="product">
-              <Link to={`/product/${p._id}`}>
-                <img className="product-image" src={p.image} alt={p.name} />
-              </Link>
-
+              <img className="product-image" src={p.image} alt={p.name} />
               <div className="product-name">
-                <Link to={`/product/${p._id}`}>{p.name}</Link>
+                <Link to={`/catalog`}>{p.name}</Link>
               </div>
-
               <div className="product-brand">{p.brand}</div>
               <div className="product-price">${p.price}</div>
-
               <div className="product-rating">
                 {p.rating} Stars ({p.numReviews} Reviews)
               </div>
@@ -30,5 +66,3 @@ function Catalog() {
     </div>
   );
 }
-
-export default Catalog;
