6e083310e6daac6f6b616f3b9a85c58fb21f93c8
York Fan <fanyuke4925@gmail.com>
2026-04-07 18:27:27 -0400

    Complete Lab 5 frontend tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main app', () => {
+  it('should render the Orders links', () => {
+    render(<App />);
+    expect(screen.getAllByText('Orders').length).toBeGreaterThan(0);
+  });
+
+  it('should render the Payment links', () => {
+    render(<App />);
+    expect(screen.getAllByText('Payment').length).toBeGreaterThan(0);
+  });
+
+  it('should render the Home links', () => {
+    render(<App />);
+    expect(screen.getAllByRole('link', { name: 'Home' }).length).toBeGreaterThan(0);
+  });
+
+  it('should render the Explore header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: 'Explore' })).toBeInTheDocument();
+  });
 });
